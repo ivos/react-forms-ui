090b7f362ae7022f708fd7cee0369a412090a52e
Ivo Maixner <ivo.maixner@gmail.com>
2015-10-28 20:47:17 +0100

    Add select locale.

--- src/app/layout/app.js
+++ src/app/layout/app.js
@@ -5,20 +5,25 @@ import Footer from './footer';
 export default React.createClass({
 
 	getInitialState(){
-		return {active: 'home'};
+		return {locale: 'en'};
 	},
 
 	render() {
 		var {pathname} = this.props.location;
 		var active = pathname.split('/')[1] || 'home';
+		var {locale} = this.state;
 		return (
 			<div>
-				<Header active={active}/>
+				<Header active={active} locale={locale} onLocaleChange={this.setLocale}/>
 
 				<div>{this.props.children}</div>
 				<Footer/>
 			</div>
 		);
+	},
+
+	setLocale(locale){
+		this.setState({locale: locale});
 	}
 
 });
--- src/app/layout/header.js
+++ src/app/layout/header.js
@@ -3,13 +3,19 @@ import React from 'react';
 export default React.createClass({
 
 	render() {
-		var {active} = this.props;
+		var {active, locale='en'} = this.props;
+		var localeLabels = {
+			en: <span><img src="img/flag-en.jpg" height="14" width="23"/>&nbsp;English</span>,
+			cz: <span><img src="img/flag-cz.jpg" height="14" width="21"/>&nbsp;Česky</span>
+		};
+		var currentLocaleLabel = localeLabels[locale];
 		return (
 			<nav className="navbar navbar-default navbar-fixed-top" role="navigation">
 				<div className="container-fluid">
+
 					<div className="navbar-header">
 						<button type="button" className="navbar-toggle" data-toggle="collapse"
-								data-target="#app-navbar-collapse">
+						        data-target="#app-navbar-collapse">
 							<span className="sr-only">Toggle navigation</span>
 							<span className="icon-bar"></span>
 							<span className="icon-bar"></span>
@@ -19,6 +25,7 @@ export default React.createClass({
 					</div>
 
 					<div id="app-navbar-collapse" className="collapse navbar-collapse">
+
 						<ul className="nav navbar-nav">
 							<li className={('home' === active)? 'active' : ''}>
 								<a id="home-link" href="#home">Home</a>
@@ -30,10 +37,45 @@ export default React.createClass({
 								<a id="partners-link" href="#partners">Partners</a>
 							</li>
 						</ul>
+
+						<ul className="nav navbar-nav navbar-right">
+							<li className="dropdown">
+								<a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button"
+								   aria-haspopup="true" aria-expanded="false">
+									{currentLocaleLabel}
+									&nbsp;<span className="caret"></span></a>
+								<ul className="dropdown-menu">
+									{'en' !== locale &&
+									<li>
+										<a href="#" onClick={this.setLocaleEn}>{localeLabels.en}</a>
+									</li>
+									}
+									{'cz' !== locale &&
+									<li>
+										<a href="#" onClick={this.setLocaleCz}>{localeLabels.cz}</a>
+									</li>
+									}
+								</ul>
+							</li>
+						</ul>
+
 					</div>
+
 				</div>
 			</nav>
 		);
+	},
+
+	setLocaleEn(event){
+		event.preventDefault();
+		var {onLocaleChange} = this.props;
+		onLocaleChange('en');
+	},
+
+	setLocaleCz(event){
+		event.preventDefault();
+		var {onLocaleChange} = this.props;
+		onLocaleChange('cz');
 	}
 
 });
