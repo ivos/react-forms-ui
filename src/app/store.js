16f546e35a818a444174c98fa07d9886ea4b28e7
Ivo Maixner <ivo.maixner@gmail.com>
2015-11-12 14:26:26 +0100

    Make store async delayed. Fix NPE in store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,30 +1,39 @@
 import State from './state';
 
+var delay = 200;
+
 export function getList(urlBase, options) {
 	console.log('STORE GET /' + urlBase + '/', options);
 	var result = State[urlBase].filter(function (item) {
 		var matches = true;
 		if (options.data) {
 			Object.keys(options.data).forEach(function (key) {
-				if (typeof item[key] === 'string' && !item[key].toLowerCase().startsWith(options.data[key].toLowerCase())) {
+				if (typeof item[key] === 'string' && options.data[key]
+					&& !item[key].toLowerCase().startsWith(options.data[key].toLowerCase())) {
 					matches = false;
 				}
 			});
 		}
 		return matches;
 	});
-	options.success(result);
+	setTimeout(function () {
+		options.success(result);
+	}, delay);
 }
 
 export function getOne(urlBase, id, options) {
 	console.log('STORE GET /' + urlBase + '/' + id, options);
-	options.success(State[urlBase][id]);
+	setTimeout(function () {
+		options.success(State[urlBase][id]);
+	}, delay);
 }
 
 export function put(urlBase, id, options) {
 	console.log('STORE PUT /' + urlBase + '/' + id, options);
 	State[urlBase][id] = options.data;
-	options.success(State[urlBase][id]);
+	setTimeout(function () {
+		options.success(State[urlBase][id]);
+	}, delay);
 }
 
 export function post(urlBase, options) {
@@ -33,5 +42,7 @@ export function post(urlBase, options) {
 	var id = array[array.length - 1].id + 1;
 	options.data.id = id;
 	array.push(options.data);
-	options.success(State[urlBase][id]);
+	setTimeout(function () {
+		options.success(State[urlBase][id]);
+	}, delay);
 }
