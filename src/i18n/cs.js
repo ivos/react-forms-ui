90c7dbd09bf13da3961bcef5dabb662d8d32dcfc
Ivo Maixner <ivo.maixner@gmail.com>
2016-10-10 09:12:16 +0200

    Fix cs plurals in new i18next version.

--- src/i18n/cs.js
+++ src/i18n/cs.js
@@ -2,11 +2,11 @@ export default {
 	validation: {
 		required: 'Povinné.',
 		minLength: 'Musí mít alespoň {{count}} znak.',
-		minLength_2: 'Musí mít alespoň {{count}} znaky.',
-		minLength_5: 'Musí mít alespoň {{count}} znaků.',
+		minLength_1: 'Musí mít alespoň {{count}} znaky.',
+		minLength_2: 'Musí mít alespoň {{count}} znaků.',
 		maxLength: 'Musí mít nejvýše {{count}} znak.',
-		maxLength_2: 'Musí mít nejvýše {{count}} znaky.',
-		maxLength_5: 'Musí mít nejvýše {{count}} znaků.',
+		maxLength_1: 'Musí mít nejvýše {{count}} znaky.',
+		maxLength_2: 'Musí mít nejvýše {{count}} znaků.',
 		min: 'Musí být nejméně {{count}}.',
 		max: 'Musí být nejvýše {{count}}.',
 		invalidFormat: 'Neplatný formát.'
